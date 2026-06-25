7577e45ff75738390ee6bc08c053c74a20e15ffc
BNilay <ybelkisnilay@gmail.com>
2026-06-25 17:24:30 +0300

    Add Ocelot API Gateway routing

--- api-gateway/ApiGateway/Program.cs
+++ api-gateway/ApiGateway/Program.cs
@@ -1,10 +1,17 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
+
+builder.Services.AddOcelot(builder.Configuration);
 
 var app = builder.Build();
 
@@ -15,7 +22,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
 app.MapControllers();
+//app.UseHttpsRedirection();
+await app.UseOcelot();
 app.Run();
 
